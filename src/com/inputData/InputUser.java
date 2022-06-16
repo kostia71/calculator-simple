d1b7706c70c12fe3ec102ce5799faf23e11fbfb6
Kostia Chaplyha <kostia71@gmail.com>
2022-06-16 11:39:52 +0300

    Update calculator-simple

--- src/com/inputData/InputUser.java
+++ src/com/inputData/InputUser.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 
 public class InputUser {
 
-    private String number1;
-    private String number2;
+    private int number1;
+    private int number2;
     private String operand;
 
     Scanner in = new Scanner(System.in);
 
-    public String getNumber1() {
+    public int getNumber1() {
         System.out.println("Введите number1: ");
-        return number1 = in.nextLine();
+        return Integer.parseInt(in.nextLine());
     }
 
 
-    public String getNumber2() {
+    public int getNumber2() {
         System.out.println("Введите number2: ");
-        return number2 = in.nextLine();
+        return Integer.parseInt(in.nextLine());
     }
 
     public String getOperand() {
--- src/com/service/Calculation.java
+++ src/com/service/Calculation.java
@@ -13,15 +13,15 @@ public class Calculation {
         operand = inputUser.getOperand();
 
         if (operand.equals("+")) {
-            result = Integer.parseInt(inputUser.getNumber1()) + Integer.parseInt(inputUser.getNumber2());
+            result = inputUser.getNumber1() + inputUser.getNumber2();
         } else if (operand.equals("-")) {
-            result = Integer.parseInt(inputUser.getNumber1()) - Integer.parseInt(inputUser.getNumber2());
+            result = inputUser.getNumber1() - inputUser.getNumber2();
         } else if (operand.equals("*")) {
-            result = Integer.parseInt(inputUser.getNumber1()) * Integer.parseInt(inputUser.getNumber2());
+            result = inputUser.getNumber1() * inputUser.getNumber2();
         } else if (operand.equals("/")) {
-            int divaded = Integer.parseInt(inputUser.getNumber2());
+            int divaded = inputUser.getNumber2();
             if (divaded != 0) {
-                result = Integer.parseInt(inputUser.getNumber1()) / divaded;
+                result = inputUser.getNumber1() / divaded;
             } else {
                 System.out.println("Делить на ноль нельзя");
             }
